ce80f1a7d011e78216aa938b18bddd0c39068244
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-02-25 23:31:31 +0500

    changed

---
+++ pages/posts/first-post.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+export default function FirstPost(){
+	return <>
+		<i>
+			<Link href="/"><a>Go to the homepage!</a></Link>
+		</i>
+		<h1>This is my first post!!!</h1>
+		</>
+}
